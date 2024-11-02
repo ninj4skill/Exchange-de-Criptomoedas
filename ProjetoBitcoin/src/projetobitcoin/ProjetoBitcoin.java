37b97468d8cb752fa978cc8bb9b1106e454cee1d
Marcelo Boalento <148659100+ninj4skill@users.noreply.github.com>
2024-11-02 02:06:09 -0300

    Add files via upload

---
+++ ProjetoBitcoin/src/projetobitcoin/ProjetoBitcoin.java
@@ -0,0 +1,18 @@
+package projetobitcoin;
+
+import view.LoginFrame;
+
+
+public class ProjetoBitcoin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginFrame lf = new LoginFrame();
+        lf.setVisible(true);
+        
+    }
+    
+}
